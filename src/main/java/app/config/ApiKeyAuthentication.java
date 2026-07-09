0fcef2c0981bafaa32e8da89c99cd049004e8655
Oleksandr Sidorov <o.sidorov@tinqin.com>
2026-07-09 23:14:57 +0300

    Notifications requests, xapikey

---
+++ src/main/java/app/config/ApiKeyAuthentication.java
@@ -0,0 +1,55 @@
+package app.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class ApiKeyAuthentication implements Authentication {
+
+    private String apiKey;
+
+    private boolean authenticated = true;
+
+    public ApiKeyAuthentication(String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public Object getCredentials() {
+        return apiKey;
+    }
+
+    @Override
+    public Object getDetails() {
+        return null;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return apiKey;
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return authenticated;
+    }
+
+    @Override
+    public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+        this.authenticated = isAuthenticated;
+    }
+
+    @Override
+    public String getName() {
+        return "API-Key";
+    }
+}
